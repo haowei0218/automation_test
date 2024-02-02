b05dba9accd6316702c85e5a8ff2074e85669068
haowei0218 <haoweizhou891@gmail.com>
2024-02-02 18:20:52 +0800

    user_sign_up

--- cypress/e2e/myassest/user_sign_up.cy.ts
+++ cypress/e2e/myassest/user_sign_up.cy.ts
@@ -19,7 +19,7 @@ describe('user Sign up', () => {
         cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiButton-label > span').click('center')
     })
 
-    it.only('Sign up with correct name', () => {
+    it('Sign up with correct name', () => {
         cy.get('.jss25 > .MuiButtonBase-root > .MuiButton-label > span').click()
         cy.get('#userName').type('yuyuyu')
         cy.get('#email').type('haoweizhou@gmail.com')
@@ -28,5 +28,38 @@ describe('user Sign up', () => {
         cy.get('#userName').type('123456789').clear()
         cy.get('#userName').type('rururu12456').clear()
     })
+    it('Sign up with missing email address', () => {
+        cy.get('.jss25 > .MuiButtonBase-root > .MuiButton-label > span').click()
+        cy.get('#userName').type('red')
+        cy.get('#password').type("Ha##$#0612")
+        cy.get('.jss62 > .MuiButtonBase-root > .MuiButton-label').click()
+        cy.get(':nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputAdornment-root > .MuiSvgIcon-root > path').should('exist')
+
+    })
+
+    context('password display/hide button', () => {
+        it('first click', () => {
+            cy.get('.jss25 > .MuiButtonBase-root > .MuiButton-label > span').click()
+            cy.get('#password').type('Ha####900218')
+            cy.get('.MuiInputAdornment-root > .MuiSvgIcon-root > path').click()
+            cy.get('#password').invoke('val').should('exist', 'Ha####900218')
+        })
+        it('second click', () => {
+            cy.get('.jss25 > .MuiButtonBase-root > .MuiButton-label > span').click()
+            cy.get('#password').type('#90%%reY')
+            cy.get('.MuiInputAdornment-root > .MuiSvgIcon-root > path').click()
+            cy.get('.MuiInputAdornment-root > .MuiSvgIcon-root > path').click()
+            cy.get('#password').invoke('val').should('exist', "......")
+        })
+
+    })
+    context.only('Sign up page Various links', () => {
+        it('Sign up page Various links', () => {
+            cy.get('.jss25 > .MuiButtonBase-root > .MuiButton-label > span').click()
+            cy.get('[href="https://myassets.com/terms-and-conditions/"]').click('center')
+            cy.get('/html/body/div[1]/section/div/div[2]/div/div[2]/div/div/a')
+        })
+    })
+
 
 })
