86eb173bdab5c229b77767d35d6da4139f6380ce
haowei0218 <haoweizhou891@gmail.com>
2024-03-08 14:44:19 +0800

    3/8

--- cypress/e2e/lesson/ex1_chear_check_select_type.cy.ts
+++ cypress/e2e/lesson/ex1_chear_check_select_type.cy.ts
@@ -4,18 +4,18 @@ describe('綜合練習', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/automation_test/cypress/index.html')
     })
-    it('type function', () => {
+    it('type commands', () => {
         cy.get('textarea').type('this is nice day')
     })
-    it('clear function', () => {
+    it('clear commands', () => {
         cy.get('textarea').clear()
     })
-    it("select function", () => {
+    it("select commands", () => {
         cy.get('select').eq(0).select('1').should('contain.text', 'red')
         cy.get('select').eq(0).select('2').should('contain.text', 'yellow')
         cy.get('select').eq(0).select('3').should('contain.text', 'orange')
     })
-    it('check function', () => {
+    it('check commands', () => {
         cy.get('[type="radio"]').check()
         cy.get('[type="checkbox"]').check(['money', 'house'])
         cy.get('[type="radio"]').check({ force: true }).should('be.checked')
